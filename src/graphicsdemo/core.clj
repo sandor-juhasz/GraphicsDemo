b4b4e120945b5e48b026b7d2b3c004170ecb4c0f
Sandor Juhasz <sandor.juhasz@ni.com>
2014-12-30 13:11:14 +0100

    Drawing rectangle

--- src/graphicsdemo/core.clj
+++ src/graphicsdemo/core.clj
@@ -3,18 +3,16 @@
 
 (defn setup []
   (q/smooth)
-  (q/frame-rate 1)
-  (q/background 200))
+  (q/frame-rate 60)
+  (q/background 200)
+
+  (q/stroke (q/random 255) (q/random 255) (q/random 255)) 
+  (q/stroke-weight (q/random 10))
+  (q/fill (q/random 255) (q/random 255) (q/random 255)) 
+  )
 
 (defn draw []
-  (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
-  (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
-  (q/fill (q/random 255))               ;; Set the fill colour to a random grey
-
-  (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
-        x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-        y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-    (q/ellipse x y diam diam)))         ;; Draw a circle at x y with the correct diameter
+  (q/rect 10 10 (- (q/width) 20) (- (q/height) 20)))
 
 (defn run []
   (q/defsketch example :title "Hello" :setup setup :draw draw :size [320 200]))
